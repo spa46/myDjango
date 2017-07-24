edc34be133715ad125de7bbde3b365ba49521798
Sean SC Park <psckyky@hotmail.com>
2017-07-24 15:33:08 +0000

    [bug fix] 	fixed spelling error

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-boostrap3',
+#    'django-bootstrap3',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
