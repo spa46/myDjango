d89a0236cfde007f3e31c33e48fe66e664c09583
Sean SC Park <psckyky@hotmail.com>
2017-08-19 06:48:45 +0000

    [FIX bug]     A bug on: return result of vote.         the url related to this function tried to find its id from <pk> but,         as this function did not use generic view; therefore, this did not need         changes from changed_id to pk.

--- polls/urls.py
+++ polls/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
     # ex: /polls/5/vote/
-    url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
